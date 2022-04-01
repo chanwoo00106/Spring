95f9e9c9ea87657cd0974075855cd05e445e4d71
변찬우 <chan00106@naver.com>
2022-04-02 01:39:21 +0900

    restart

---
+++ src/main/kotlin/com/example/srud/SrudApplication.kt
@@ -0,0 +1,11 @@
+package com.example.srud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SrudApplication
+
+fun main(args: Array<String>) {
+    runApplication<SrudApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/srud/SrudApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.srud
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SrudApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
