d6673d91789fab466bce53e175f841a1a02d74e0
변찬우 <chan00106@naver.com>
2022-04-17 15:34:15 +0900

    Add 회원 service

---
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,37 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 회원 가입
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+
+        this.memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        this.memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다");
+                });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return this.memberRepository.findAll();
+    }
+
+    // 특정 회원 조회
+    public Optional<Member> findOne(Long memberId) {
+        return this.memberRepository.findById(memberId);
+    }
+}
