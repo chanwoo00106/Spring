1fbc0cda2e4a621ec7138b517d9805bd9f30035f
변찬우 <chan00106@naver.com>
2022-04-18 10:38:25 +0900

    Add auto dependency injection

---
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,13 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+    }
+}
