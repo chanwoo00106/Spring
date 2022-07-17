8ac196bada9caeb21b5dbaddee9ab37d4f97628b
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-07-17 22:11:01 +0900

    Add AutoAppConfig.java

--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,9 +2,10 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RateDiscountPolicy implements DiscountPolicy{
-
 
     private int discountPercent = 10;
     @Override
