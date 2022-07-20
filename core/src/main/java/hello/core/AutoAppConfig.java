840086ee0b63ccabedc6185b33e777e8ba0752ad
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-07-20 22:39:49 +0900

    Component filter test

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core", // default 값은 이 클래스의 시작 위치 이다
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
