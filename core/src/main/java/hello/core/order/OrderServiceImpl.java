b749c3a7b1eacab5cef2fcc68e20b62183009c0d
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-07-29 15:27:25 +0900

    조회한 빈이 모두 필요할 때, List, Map

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,7 +5,6 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 
