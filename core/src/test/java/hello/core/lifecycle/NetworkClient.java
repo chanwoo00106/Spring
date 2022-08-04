c66eecfd9a32f7a671d04af028da80dfe5eb3f71
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-08-04 15:22:25 +0900

    애노테이션 @PostConstruct, @PreDestroy

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
     private String url;
 
@@ -23,11 +26,13 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
+    @PostConstruct
     public void init() throws Exception {
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() throws Exception {
         disconnect();
     }
