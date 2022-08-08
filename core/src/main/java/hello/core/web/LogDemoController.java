e56662dec340c5199f178a500928a8d0139cb370
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-08-09 08:53:03 +0900

    스코프와 프록시

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -13,14 +13,12 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURI().toString();
-
-        MyLogger myLogger = myLoggerProvider.getObject();
 
         myLogger.setRequestURL(requestURL);
 
