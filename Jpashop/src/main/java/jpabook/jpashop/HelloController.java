dc38a1a9e1587e42c9087325618aa64f8a4a2d24
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-08-30 20:43:08 +0900

    JPA와 DB 설정

--- Jpashop/src/main/java/jpabook/jpashop/HelloController.java
+++ Jpashop/src/main/java/jpabook/jpashop/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HelloCOntroller {
+public class HelloController {
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
