64bf83e3e3dd98921f709912b71cc72ece96666f
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-09-07 23:48:41 +0900

    Add member test

---
+++ Jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,52 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("chan");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception {
+        // given
+        Member member1 = new Member();
+        member1.setName("chan");
+
+        Member member2 = new Member();
+        member2.setName("chan");
+
+
+        // when
+        memberService.join(member1);
+        memberService.join(member2);
+
+        // then
+        fail("같은 이름은 존재할 수 없습니다");
+    }
+}
