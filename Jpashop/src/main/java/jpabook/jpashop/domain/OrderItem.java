babcb359d7b6d79936305dd5c6d6c4d6af98f025
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-09-13 22:18:52 +0900

    Add order service

--- Jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ Jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,13 +1,16 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
     @Id @GeneratedValue
     @Column(name = "order_item_id")
@@ -23,6 +26,8 @@ public class OrderItem {
 
     private int orderPrice;
     private int count;
+
+//    protected OrderItem() {}
 
     //==생성 메서드==//
     public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
