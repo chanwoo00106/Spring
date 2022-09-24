9623eb4ab2903fbcbab11a411f0005c3d95c6129
chanwoo00106 <chan00106@naver.com>
2022-09-24 23:08:25 +0900

    Revise jpashop

---
+++ revise/src/main/kotlin/revise/revise/ReviseApplication.kt
@@ -0,0 +1,11 @@
+package revise.revise
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReviseApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReviseApplication>(*args)
+}
