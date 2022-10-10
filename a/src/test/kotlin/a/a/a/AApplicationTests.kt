2c08351da81c8017666ca3a0fb98abab571f337e
chanwoo00106 <chan00106@naver.com>
2022-10-10 19:56:04 +0900

    Begin a

---
+++ a/src/test/kotlin/a/a/a/AApplicationTests.kt
@@ -0,0 +1,13 @@
+package a.a.a
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
