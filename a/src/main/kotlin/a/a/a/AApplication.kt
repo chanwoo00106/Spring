c9b12718c926b199896125be3b3cbd21712b31b3
chanwoo00106 <chan00106@naver.com>
2022-10-16 23:36:05 +0900

    Inject test data

--- a/src/main/kotlin/a/a/a/AApplication.kt
+++ a/src/main/kotlin/a/a/a/AApplication.kt
@@ -1,10 +1,33 @@
 package a.a.a
 
+import a.a.a.domain.Role
+import a.a.a.domain.User
+import a.a.a.dto.RoleToUserDto
+import a.a.a.service.UserServiceImpl
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class AApplication
+class AApplication {
+	@Bean
+	fun run(userService: UserServiceImpl): CommandLineRunner {
+		return CommandLineRunner { args ->
+			userService.saveRole(Role("ROLE_USER"))
+			userService.saveRole(Role("ROLE_MEMBER"))
+			userService.saveRole(Role("ROLE_ADMIN"))
+			userService.saveRole(Role("ROLE_SUPER_ADMIN"))
+
+			userService.saveUser(User("chan", "hello", "world", null))
+			userService.saveUser(User("Teemo", "hell2", "world2", null))
+			userService.saveUser(User("hahaha", "hello3", "world3", null))
+			userService.saveUser(User("they1234", "hello4", "world4", null))
+
+			userService.addRoleToUser(RoleToUserDto("hello", "ROLE_USER"))
+		}
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<AApplication>(*args)
