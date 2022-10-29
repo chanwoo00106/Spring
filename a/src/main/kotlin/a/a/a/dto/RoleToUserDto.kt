50817eb98b42918433d1819633af9625f4c222e9
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-10-29 21:15:01 +0900

    Delete All

--- a/src/main/kotlin/a/a/a/dto/RoleToUserDto.kt
+++
@@ -1,6 +0,0 @@
-package a.a.a.dto
-
-class RoleToUserDto (
-    var username: String,
-    var roleName: String
-)
--- a/src/main/kotlin/a/a/a/repository/RoleRepository.kt
+++
@@ -1,8 +0,0 @@
-package a.a.a.repository
-
-import a.a.a.domain.Role
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface RoleRepository: JpaRepository<Role, Long> {
-    fun findByName(name: String): Role
-}
