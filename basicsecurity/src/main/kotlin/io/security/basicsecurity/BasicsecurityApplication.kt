ecc08f7f83e9bae19f3a52bdd018db0488c70eb3
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-11-01 20:58:44 +0900

    Begin basic security

---
+++ basicsecurity/src/main/kotlin/io/security/basicsecurity/BasicsecurityApplication.kt
@@ -0,0 +1,11 @@
+package io.security.basicsecurity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BasicsecurityApplication
+
+fun main(args: Array<String>) {
+	runApplication<BasicsecurityApplication>(*args)
+}
