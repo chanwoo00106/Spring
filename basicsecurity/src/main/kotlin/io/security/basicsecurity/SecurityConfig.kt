98ebc3f3fc9b710ec7eacf9047b6f6235c6c6a2a
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-11-05 19:18:08 +0900

    Add 로그아웃 처리

--- basicsecurity/src/main/kotlin/io/security/basicsecurity/SecurityConfig.kt
+++ basicsecurity/src/main/kotlin/io/security/basicsecurity/SecurityConfig.kt
@@ -2,12 +2,9 @@ package io.security.basicsecurity
 
 import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.core.Authentication
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 import org.springframework.stereotype.Component
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
+import javax.servlet.http.HttpSession
 
 @Component
 class SecurityConfig {
@@ -34,6 +31,20 @@ class SecurityConfig {
                 response.sendRedirect("/login")
             }
             .permitAll()
+            .and()
+            .logout()
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/login")
+            .addLogoutHandler {
+                request, response, authentication ->
+                val session = request.getSession()
+                session.invalidate()
+            }
+            .logoutSuccessHandler {
+                request, response, authentication ->
+                response.sendRedirect("/login")
+            }
+            .deleteCookies("remember=me")
             .and()
             .build()
     }
