1340e1d92b17a849f833b957b2f1e5a7665615d8
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-11-06 23:48:12 +0900

    add 엔티티 맵핑 연습

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,21 +1,15 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Member {
-    public Member() {
-    }
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    @Id
+    // IDENTITY 데이터베이스가 자동으로 할당해줌, 때문에 em.persist 시점에 바로 쿼리가 날라감
+    // SEQUENCE 데이터메이스한테 다음 값을 물어보기 때문에 지연 로딩이 가능함
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name", nullable = false)
     private String name;
 
     public Long getId() {
