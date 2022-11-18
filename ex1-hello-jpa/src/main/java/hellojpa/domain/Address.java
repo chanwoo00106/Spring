5ff80618af2c2c4d98a3a8e1e51a1e242d7e77af
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-11-18 11:44:41 +0900

    add address equals

--- ex1-hello-jpa/src/main/java/hellojpa/domain/Address.java
+++ ex1-hello-jpa/src/main/java/hellojpa/domain/Address.java
@@ -1,6 +1,7 @@
 package hellojpa.domain;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class Address {
@@ -39,5 +40,18 @@ public class Address {
 
     public void setZipcode(String zipcode) {
         this.zipcode = zipcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(getCity(), address.getCity()) && Objects.equals(getStreet(), address.getStreet()) && Objects.equals(getZipcode(), address.getZipcode());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCity(), getStreet(), getZipcode());
     }
 }
