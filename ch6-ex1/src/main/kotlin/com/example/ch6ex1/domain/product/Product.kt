d1fa58094e9b7354950dcc635a46d7eca5705607
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-01-05 22:42:15 +0900

    Update Product entity

--- ch6-ex1/src/main/kotlin/com/example/ch6ex1/domain/product/Product.kt
+++ ch6-ex1/src/main/kotlin/com/example/ch6ex1/domain/product/Product.kt
@@ -1,5 +1,6 @@
 package com.example.ch6ex1.domain.product
 
+import com.example.ch6ex1.domain.product.enums.Currency
 import javax.persistence.*
 
 @Entity
@@ -10,8 +11,8 @@ class Product (
   @Column(nullable = false, length = 45)
   val price: Int,
 
-  @Column(nullable = false, length = 45)
-  val currency: String
+  @Enumerated(value = EnumType.STRING)
+  val currency: Currency
 ) {
   @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
   val id: Long = 0
---
+++ ch6-ex1/src/main/kotlin/com/example/ch6ex1/domain/product/enums/Currency.kt
@@ -0,0 +1,5 @@
+package com.example.ch6ex1.domain.product.enums
+
+enum class Currency {
+  USD, GBP, EUR
+}
