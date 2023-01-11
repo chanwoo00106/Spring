b13a47e74f5d6ec08228aa5fc0ac14388100c2b4
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-01-11 17:00:09 +0900

    solve cors problem

--- cors-ex1/src/main/kotlin/com/example/corsex1/MainController.kt
+++ cors-ex1/src/main/kotlin/com/example/corsex1/MainController.kt
@@ -1,6 +1,7 @@
 package com.example.corsex1
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.ResponseBody
@@ -12,6 +13,7 @@ class MainController {
     return "main.html"
   }
 
+//  @CrossOrigin("http://127.0.0.1:8080", "http://localhost:8080")
   @PostMapping("/test")
   @ResponseBody
   fun test(): String {
---
+++ cors-ex1/src/main/kotlin/com/example/corsex1/WebMvcConfig.kt
@@ -0,0 +1,13 @@
+package com.example.corsex1
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig: WebMvcConfigurer{
+  override fun addCorsMappings(registry: CorsRegistry) {
+    registry.addMapping("/**")
+      .allowedOrigins("http://localhost:8080", "http://172.0.0.1:8080")
+  }
+}
