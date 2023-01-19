976f30ab62314f25991d30247a29fcad7da37b07
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-01-19 23:22:51 +0900

    Add 유저 조회

--- ch12-ex1/src/main/kotlin/com/example/ch12ex1/MainController.kt
+++ ch12-ex1/src/main/kotlin/com/example/ch12ex1/MainController.kt
@@ -1,12 +1,16 @@
 package com.example.ch12ex1
 
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import java.util.logging.Logger
 
 @Controller
 class MainController {
+  private val logger = Logger.getLogger(MainController::class.java.name)
   @GetMapping
-  fun main(): String {
+  fun main(token: OAuth2AuthenticationToken): String {
+    logger.info(token.principal.toString())
     return "main.html"
   }
 }
