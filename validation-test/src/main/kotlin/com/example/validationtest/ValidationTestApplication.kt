ec2446f785714e302ba99dcf0e7a1bf28252836f
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-04-01 21:58:27 +0900

    Begin project validation test

---
+++ validation-test/src/main/kotlin/com/example/validationtest/ValidationTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.validationtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ValidationTestApplication
+
+fun main(args: Array<String>) {
+  runApplication<ValidationTestApplication>(*args)
+}
